ae78ec1ac69dedefde1912292e045d172dd2133a
Jainam C Shah <jainamcshah98@gmail.com>
2018-10-15 19:19:07 +0530

    Update sql file and screens

--- classroom.sql
+++ classroom.sql
@@ -33,16 +33,6 @@ CREATE TABLE `articles` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `articles`
---
-
-LOCK TABLES `articles` WRITE;
-/*!40000 ALTER TABLE `articles` DISABLE KEYS */;
-INSERT INTO `articles` VALUES (1,'Lecture by J. K. Rowling on 5th September','jcs98','<p>lorem impsum&nbsp;dolor si amet</p><p>testing editting</p>','2018-10-09 16:26:53'),(2,'Tomorrow\'s Particle Physics lecture canceled','jcs98','<p>The quick brown fow jumped over the lazy dogs</p>','2018-10-09 16:26:39'),(3,'Assignment on Relativity','Prof. Einstein','<p>Prove E=mc^2 and explain general relativity</p>','2018-10-09 14:29:28'),(4,'Double Slit Experiment','Prof. Einstein','<p>Draw the diagram and give proof.</p><p><a href=\"https://en.wikipedia.org/wiki/Double-slit_experiment\">https://en.wikipedia.org/wiki/Double-slit_experiment</a></p>','2018-10-09 16:19:39');
-/*!40000 ALTER TABLE `articles` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `comments`
 --
 
@@ -60,14 +50,36 @@ CREATE TABLE `comments` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `comments`
+-- Table structure for table `submissions`
 --
 
-LOCK TABLES `comments` WRITE;
-/*!40000 ALTER TABLE `comments` DISABLE KEYS */;
-INSERT INTO `comments` VALUES (12,'jcs98','testing the comment',1,'2018-10-09 13:36:15'),(13,'jcs98','when do we need to submit this?',2,'2018-10-09 13:53:26'),(16,'jcs98','when do we need to submit this?',1,'2018-10-09 14:17:44'),(17,'jcs98','Which brown fox?',2,'2018-10-09 14:22:45'),(18,'jcs98','How many dogs?',2,'2018-10-09 14:22:58'),(20,'jcs98','Are diagrams required?',3,'2018-10-09 16:27:36'),(21,'jcs98','When do we need to submit this?',3,'2018-10-09 16:27:49'),(22,'Prof. Einstein','Yes diagrams are required.',3,'2018-10-09 16:28:29'),(23,'Prof. Einstein','Submit it by Wednesday.',3,'2018-10-09 16:28:48');
-/*!40000 ALTER TABLE `comments` ENABLE KEYS */;
-UNLOCK TABLES;
+DROP TABLE IF EXISTS `submissions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `submissions` (
+  `assignment_id` int(11) DEFAULT NULL,
+  `student_name` varchar(50) DEFAULT NULL,
+  `submit_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `teachers`
+--
+
+DROP TABLE IF EXISTS `teachers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `teachers` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `username` varchar(30) DEFAULT NULL,
+  `password` varchar(100) DEFAULT NULL,
+  `register_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `users`
@@ -84,18 +96,8 @@ CREATE TABLE `users` (
   `password` varchar(100) DEFAULT NULL,
   `register_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `users`
---
-
-LOCK TABLES `users` WRITE;
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'Jainam Shah','jcshah98@gmail.com','jcs98','$5$rounds=535000$NV.hb1awfhZierAi$tn9QqnjxtSyrB//rmEMvYODyVTwuyh2A6SrWK7kCQb/','2018-10-05 07:12:33'),(2,'Albert Einstein','relativity@example.com','Prof. Einstein','$5$rounds=535000$Ur87YCsyXs6pQ8KN$f5vNCCq9kg/E37EpO7cSMMHppUPTFv0kkHEszID7Yt4','2018-10-09 14:28:13');
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
-UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -106,4 +108,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-10-09 22:38:02
+-- Dump completed on 2018-10-15 19:08:52
